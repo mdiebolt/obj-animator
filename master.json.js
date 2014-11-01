717d7294b0286f251fc4665147e77966dd0ecaae
Matt Diebolt <mdiebolt@gmail.com>
2014-11-01 11:51:58 -0500

    :clock10::minibus: Updated in browser at strd6.github.io/editor

---
+++ master.json.js
@@ -0,0 +1,37 @@
+window["mdiebolt/obj-animator:master"]({
+  "source": {
+    "README.md": {
+      "path": "README.md",
+      "content": "",
+      "mode": "100644",
+      "type": "blob"
+    },
+    "style.styl": {
+      "path": "style.styl",
+      "content": "body\n  margin: 0\n  overflow: hidden\n",
+      "mode": "100644"
+    }
+  },
+  "distribution": {
+    "style": {
+      "path": "style",
+      "content": "module.exports = \"body {\\n  margin: 0;\\n  overflow: hidden;\\n}\";",
+      "type": "blob"
+    }
+  },
+  "progenitor": {
+    "url": "http://www.danielx.net/editor/"
+  },
+  "entryPoint": "main",
+  "repository": {
+    "branch": "master",
+    "default_branch": "master",
+    "full_name": "mdiebolt/obj-animator",
+    "homepage": null,
+    "description": "Animate models rendered from .obj data format",
+    "html_url": "https://github.com/mdiebolt/obj-animator",
+    "url": "https://api.github.com/repos/mdiebolt/obj-animator",
+    "publishBranch": "gh-pages"
+  },
+  "dependencies": {}
+});
